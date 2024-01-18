edae45b8f80c718afd50de2d29c6e1a9ab3f7a79
Danny Thanakrit <pb.danny@gmail.com>
2024-01-19 06:56:53 +0700

    split scoring - matching

--- ps_matching.r
+++ ps_matching.r
@@ -1,6 +1,7 @@
 library(dplyr)
 library(tidyr)
 library(arrow)
+library(MatchIt)
 
 #--- Read partitioned parquet
 ds <- open_dataset("data/feat.parquet")
@@ -10,10 +11,43 @@ df <- as.data.frame(at) %>%
   # reverse onehot encoding -> truprice
   pivot_longer(most_price_driven:price_neutral, names_to = "truprice", values_to = "flag") %>%
   # rename column with special char
-  rename_with(make.names) %>% 
+  rename_with(make.names) %>%
   mutate(ty_brand_sales = sum(feat_brand_ty_h1_sum.sales., feat_brand_ty_h2_sum.sales.)) %>%
-  select(-c(flag, feat_brand_ty_h1_sum.sales.:feat_brand_ty_h2_sum.visits., aisle_flag, ty_flag))
+  select(-c(flag, , aisle_flag, ty_flag)) %>%
+  # select(-c(flag, feat_brand_ty_h1_sum.sales.:feat_brand_ty_h2_sum.visits., aisle_flag, ty_flag))
+  mutate(feat_ly_h1_sum.sales. = sum(pick((starts_with("feat_ly") & ends_with("h1_sum.sales."))))) %>%
+  mutate(feat_ly_h2_sum.sales. = sum(pick((starts_with("feat_ly") & ends_with("h2_sum.sales."))))) %>%
+  mutate(feat_ty_h1_sum.sales. = sum(pick((starts_with("feat_ty") & ends_with("h1_sum.sales."))))) %>%
+  mutate(feat_ty_h2_sum.sales. = sum(pick((starts_with("feat_ty") & ends_with("h2_sum.sales."))))) %>%
 
+  mutate(feat_cate_ly_h1_sum.sales. = sum(pick((starts_with("feat_cate_ly") & ends_with("h1_sum.sales."))))) %>%
+  mutate(feat_cate_ly_h2_sum.sales. = sum(pick((starts_with("feat_cate_ly") & ends_with("h2_sum.sales."))))) %>%
+  mutate(feat_cate_ty_h1_sum.sales. = sum(pick((starts_with("feat_cate_ty") & ends_with("h1_sum.sales."))))) %>%
+  mutate(feat_cate_ty_h2_sum.sales. = sum(pick((starts_with("feat_cate_ty") & ends_with("h2_sum.sales."))))) %>%
+
+  select(household_id, group:feat_cate_ty_h2_sum.sales., starts_with("feat_aisle") & ends_with("sum.visits."))
+
+
+# create  formular from all column name
+ps_formular <- df %>%
+  select(-c(household_id, ty_brand_sales, store_flag)) %>%
+  names() %>%
+  reformulate(, response = "store_flag")
+
+#---- Speed up MatchIt Option 1 : pre-run score
+#step 1 : pre-run score
+df$myfit <- fitted(glm(ps_formular, data = df, family = "binomial"))
+
+#step 2 : trim data
+trimmed_data <- df %>% select(household_id, myfit, store_flag)
+
+#step 3
+m_out <- matchit(store_flag ~ household_id, data = trimmed_data, method = "nearest", distance = trimmed_data$myfit)
+matched_unique_ids_etc <- match.data(m_out, data = trimmed_data)
+matched_unique_ids <- select(matched_unique_ids_etc, household_id)
+matched_data <- matched_unique_ids %>% inner_join(df)
+
+#---- Speed up MatchIt Option 2 : Split test, match all control (match with replacement)
 # split control
 df0 <- df[df$store_flag == 0, ]
 
@@ -21,14 +55,6 @@ df0 <- df[df$store_flag == 0, ]
 n <- 20
 df1 <- df[df$store_flag == 1, ]
 split_list <- split(df1, factor(sort(rank(row.names(df1)) %% n)))
-
-# create ps formular from all column name
-ps_formular <- df %>%
-          select(-c(household_id, ty_brand_sales, store_flag)) %>%
-          names() %>%
-          reformulate(, response = "store_flag")
-
-library(MatchIt)
 
 psm <- matchit(ps_formular, data = rbind(df0, split_list[[1]]))
 
@@ -49,7 +75,7 @@ psm_data[psm_data$subclass == 799, ]
 # suppress e+ notation
 options(scipen=999)
 
-#----Code to loop all the split list, run psm matching and append
+# Code to loop all the split list, run psm matching and append
 # create new dataframes of each test split + combine with all control
 
 combined <- list()
